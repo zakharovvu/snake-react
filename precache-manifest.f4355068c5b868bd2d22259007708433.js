05431e690abf8dd64d3ecaaeb658547648d66fdc
Zakharov Viacheslav <zakharovvu@gmail.com>
2019-03-01 15:37:10 +0200

    Updates

---
+++ precache-manifest.f4355068c5b868bd2d22259007708433.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "41ad7da39cb46abc0e4e",
+    "url": "/snake-react/static/css/main.7b71478e.chunk.css"
+  },
+  {
+    "revision": "41ad7da39cb46abc0e4e",
+    "url": "/snake-react/static/js/main.41ad7da3.chunk.js"
+  },
+  {
+    "revision": "22c2d1d56ec71f86e3cf",
+    "url": "/snake-react/static/js/runtime~main.22c2d1d5.js"
+  },
+  {
+    "revision": "5bf9b2cda7c63b962686",
+    "url": "/snake-react/static/js/2.5bf9b2cd.chunk.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/snake-react/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "f56ccb09e8a70734c89fd20269662f7a",
+    "url": "/snake-react/index.html"
+  }
+];
---
+++ service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+importScripts(
+  "/snake-react/precache-manifest.f4355068c5b868bd2d22259007708433.js"
+);
+
+workbox.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute("/snake-react/index.html", {
+  
+  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+});
